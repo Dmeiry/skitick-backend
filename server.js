7e28bf17fcdc303e813ccca311128495631869d5
Nasser Dmeiry <nasser.dmeiry@gmail.com>
2024-08-24 17:23:11 +0300

    worked with insert,update,delete

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv/config";
+
 import requestLogger from "./middleware/requestLogger.js";
 import errorHandler from "./middleware/errorHandler.js";
-import userRoutes from "./routes/userRoutes.js"
-import connectDB from "./config/db.js"
-import dotenv from "dotenv/config";
+import userRoutes from "./routes/userRoutes.js";
+import stickersRoutes from "./routes/stickersRoutes.js";
+import connectDB from "./config/db.js";
+
 
 // constants
 const PORT = process.env.PORT || 3000;
@@ -26,6 +29,7 @@ app.get("/", (req, res) => {
 
 // routes
 app.use("/api/users", userRoutes);
+app.use("/api/stickers", stickersRoutes);
 
 // error handling middlware
 app.use(errorHandler);
