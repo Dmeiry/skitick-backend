6405fbe6d35adde79603f21fb8fe78b2a891fc08
MahmoudDawoodSWE <mahmouddawood.swe@gmail.com>
2024-08-28 22:38:49 +0300

    add the my answers array to the user board array

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,57 +1,85 @@
 import { Schema, model } from "mongoose";
 
 const userSchema = new Schema(
-    {
-
-        phone: {
-            type: String,
-            validate: {
-                validator: function (value) {
-                    // Regular expression to validate phone number
-                    const phoneRegex = /^\+?[1-9]\d{1,14}$/;
-                    return phoneRegex.test(value);
-                },
-                message: "Invalid phone number",
-            },
+  {
+    phone: {
+      type: String,
+      validate: {
+        validator: function (value) {
+          // Regular expression to validate phone number
+          const phoneRegex = /^\+?[1-9]\d{1,14}$/;
+          return phoneRegex.test(value);
         },
-        auth: {
-            type: Boolean,
-            required: true,
-        },
-        class: {
-            type: String,
-        },
-        currentBoard: {
-            type: String,
-        },
-        firstname: {
-            type: String,
-            required: true,
-        },
-        imageUrl: {
-            type: String,
-        },
-        lastname: {
-            type: String,
-            required: true,
-        },
-        myBoards: {
-            type: [String],
-            default: [],
-        },
-        myStickers: {
-            type: [String],
-            default: [],
-        },
-        score: {
-            type: Number,
-        },
-        status: {
-            type: String,
-            default: 1,
-        },
+        message: "Invalid phone number",
+      },
     },
-    { timestamps: true }
+    auth: {
+      type: Boolean,
+      required: true,
+    },
+    class: {
+      type: String,
+    },
+    currentBoard: {
+      type: Schema.Types.ObjectId,
+      ref: "board",
+    },
+    firstname: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    myBoards: {
+      type: [
+        {
+          board: {
+            type: Schema.Types.ObjectId,
+            ref: "board",
+          },
+          progress: {
+            type: Number,
+            default: 0,
+          },
+          answers: {
+            type: Map,
+            of: Schema.Types.Mixed, // Allows storing different types of answers
+            default: new Map(),
+            // You can add a validation function to enforce that keys are ObjectId strings
+            validate: {
+              validator: function (map) {
+                for (let key of map.keys()) {
+                  if (!Schema.Types.ObjectId.isValid(key)) {
+                    return false;
+                  }
+                }
+                return true;
+              },
+              message: "All keys in answers must be valid ObjectId strings",
+            },
+          },
+        },
+      ],
+      default: [],
+    },
+    myStickers: {
+      type: [String],
+      default: [],
+    },
+    score: {
+      type: Number,
+    },
+    status: {
+      type: String,
+      default: 1,
+    },
+  },
+  { timestamps: true }
 );
 
 const User = model("user", userSchema, "users");
