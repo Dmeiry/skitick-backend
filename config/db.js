65c59011e345c4e7741ff1fb79ebf1540a8c7037
Nasser Dmeiry <nasser.dmeiry@gmail.com>
2024-09-23 11:26:17 +0300

    add cloudbuild and dockerfile, fix the db connection to enable google secrets

--- config/db.js
+++ config/db.js
@@ -1,12 +1,39 @@
 import mongoose from "mongoose";
+import { SecretManagerServiceClient } from "@google-cloud/secret-manager";
+
+const client = new SecretManagerServiceClient();
+
+const getSecretValue = async (secretName) => {
+    const [version] = await client.accessSecretVersion({
+        name: `projects/1027309556923/secrets/${secretName}/versions/latest`,
+    });
+    return version.payload.data.toString("utf8");
+};
 
 const connectDB = async () => {
     try {
-        const db = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected : ${db.connection.host}`);
+        const mongoURI = await getSecretValue("MONGO_URI"); // Using "MONGO_URI" as the secret name
+        const db = await mongoose.connect(mongoURI);
+        console.log(`MongoDB Connected: ${db.connection.host}`);
     } catch (error) {
         console.error(error);
     }
 };
 
 export default connectDB;
+
+
+// all below is the oldest , depends on .env file 
+
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//     try {
+//         const db = await mongoose.connect(process.env.MONGO_URI);
+//         console.log(`MongoDB Connected : ${db.connection.host}`);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// };
+
+// export default connectDB;
